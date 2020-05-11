b34c2cbafc98d2e19e829cd8d495ff19b171ba2e
Jocelyn Beedie <jellonator00@gmail.com>
2020-05-11 03:51:01 -0400

    Can now load and list files from Chum data

--- Gui/Test.gd
+++ Gui/Test.gd
@@ -4,9 +4,18 @@ const HelloWorld := preload("res://gdchum/ChumArchive.gdns")
 
 var thing
 
+onready var node_NGC := $GridContainer/NGC
+onready var node_DGC := $GridContainer/DGC
+
 func _ready():
 	thing = HelloWorld.new()
 
 func _input(event):
 	if event.is_action_pressed("ui_home"):
-		thing.do_thing()
+		print("START")
+		for file in thing.get_file_list():
+			print(file)
+		print("END")
+
+func _on_Button_pressed():
+	thing.load(node_NGC.text, node_DGC.text)
--- gdchum/src/lib.rs
+++ gdchum/src/lib.rs
@@ -1,14 +1,50 @@
 use gdnative::*;
 use libchum;
+use std::fs::File;
 
 #[derive(NativeClass)]
 #[inherit(Resource)]
-pub struct ChumArchive;
+pub struct ChumArchive {
+    pub archive: Option<libchum::ChumArchive>
+}
 
 #[methods]
 impl ChumArchive {
     fn _init(_owner: Resource) -> Self {
-        ChumArchive
+        ChumArchive {
+            archive: None
+        }
+    }
+
+    #[export]
+    fn load(&mut self, _owner: Resource, ngcpath: gdnative::GodotString, dgcpath: gdnative::GodotString) -> Result<(), i64> {
+        let mut ngcfile = match File::open(ngcpath.to_string()) {
+            Ok(x) => x,
+            Err(_) => return Err(gdnative::GodotError::FileBadPath as i64)
+        };
+        let mut dgcfile = match File::open(dgcpath.to_string()) {
+            Ok(x) => x,
+            Err(_) => return Err(gdnative::GodotError::FileBadPath as i64)
+        };
+        match libchum::ChumArchive::read_chum_archive(&mut ngcfile, &mut dgcfile) {
+            Ok(x) => {
+                self.archive = Some(x);
+                Ok(())
+            }
+            Err(_) => Err(gdnative::GodotError::FileCantOpen as i64)
+        }
+    }
+
+    #[export]
+    fn get_file_list(&self, _owner: Resource) -> gdnative::VariantArray {
+        let mut arr = gdnative::VariantArray::new();
+        if let Some(archive) = &self.archive {
+            for file in archive.get_files() {
+                let gdvariant = gdnative::Variant::from(file.get_name_id());
+                arr.push(&gdvariant);
+            }
+        }
+        return arr
     }
 
     #[export]

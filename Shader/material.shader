bc452c41d582da9948724dbcb3e48d6311f45505
Jocelyn Beedie <jellonator00@gmail.com>
2020-06-07 20:36:12 -0400

    Materials now use a ShaderMaterial

---
+++ Shader/material.shader
@@ -0,0 +1,31 @@
+shader_type spatial;
+render_mode depth_draw_always;
+
+uniform vec3 arg_color = vec3(1, 1, 1);
+uniform float arg_alpha = 1.0;
+uniform mat4 arg_texcoord_transform = mat4(1);
+uniform sampler2D arg_texture;
+uniform sampler2D arg_reflection;
+uniform bool has_texture = false;
+uniform bool has_reflection = false;
+
+void fragment() {
+	vec4 uvpos = vec4(UV.x, UV.y, 1.0, 0.0);
+	uvpos = arg_texcoord_transform * uvpos;
+	vec4 col1;
+	if (has_texture) {
+		col1 = texture(arg_texture, uvpos.xy);
+	} else {
+		col1 = vec4(1, 1, 1, 1);
+	}
+	vec4 col2;
+	if (has_reflection) {
+		col2 = textureProj(arg_reflection, reflect(VIEW, normalize(NORMAL)));
+	} else {
+		col2 = vec4(0, 0, 0, 0);
+	}
+	vec4 outcol = col1 * vec4(arg_color, arg_alpha);
+	outcol.rgb += col2.rgb * col2.a;
+	ALBEDO = outcol.rgb;
+	ALPHA = outcol.a;
+}
--- gdchum/src/reader/readermaterial.rs
+++ gdchum/src/reader/readermaterial.rs
@@ -17,33 +17,77 @@ pub fn read_material(
             return None;
         }
     };
-    let mut material = SpatialMaterial::new();
+    let mut material = ShaderMaterial::new();
+    let shader: Resource = ResourceLoader::godot_singleton()
+        .load(
+            "res://Shader/material.shader".into(),
+            "Shader".into(),
+            false,
+        )
+        .unwrap();
+    material.set_shader(Some(shader.cast().unwrap()));
     let archiveinstance = file.get_archive_instance();
     archiveinstance
         .map(|archive, res| {
-            if let Some(texturefile) = archive.get_file_from_hash(res, matdata.get_texture()) {
-                let texturedict = reader.read_bitmap_nodeless(texturefile);
-                if texturedict.get(&"exists".into()) == true.into() {
-                    godot_print!("Found material for {}", matdata.get_texture());
-                    let image: Image = texturedict.get(&"bitmap".into()).try_to_object().unwrap();
-                    let mut texture: ImageTexture = ImageTexture::new();
-                    texture.create_from_image(Some(image), 0);
-                    material.set_texture(
-                        SpatialMaterial::TEXTURE_ALBEDO,
-                        Some(texture.cast().unwrap()),
-                    );
-                    material.set_feature(
-                        SpatialMaterial::FEATURE_TRANSPARENT,
-                        texturedict.get(&"hasalpha".into()).to_bool(),
-                    );
+            if matdata.get_texture() != 0 {
+                if let Some(texturefile) =
+                    archive.get_file_from_hash(res.clone(), matdata.get_texture())
+                {
+                    let texturedict = reader.read_bitmap_nodeless(texturefile);
+                    if texturedict.get(&"exists".into()) == true.into() {
+                        godot_print!("Found material for {}", matdata.get_texture());
+                        let image: Image =
+                            texturedict.get(&"bitmap".into()).try_to_object().unwrap();
+                        let mut texture: ImageTexture = ImageTexture::new();
+                        texture.create_from_image(Some(image), 0);
+                        material.set_shader_param("has_texture".into(), true.into());
+                        material.set_shader_param("arg_texture".into(), texture.into());
+                    } else {
+                        godot_warn!("Material {} has invalid bitmap", matdata.get_texture());
+                    }
                 } else {
-                    godot_warn!("Material {} has invalid bitmap", matdata.get_texture());
+                    godot_warn!("Material {} could not be found", matdata.get_texture());
                 }
-            } else {
-                godot_warn!("Material {} could not be found", matdata.get_texture());
+            }
+            if matdata.texture_reflection != 0 {
+                if let Some(texturefile) =
+                    archive.get_file_from_hash(res, matdata.texture_reflection)
+                {
+                    let texturedict = reader.read_bitmap_nodeless(texturefile);
+                    if texturedict.get(&"exists".into()) == true.into() {
+                        godot_print!("Found material for {}", matdata.texture_reflection);
+                        let image: Image =
+                            texturedict.get(&"bitmap".into()).try_to_object().unwrap();
+                        let mut texture: ImageTexture = ImageTexture::new();
+                        texture.create_from_image(Some(image), 0);
+                        material.set_shader_param("has_reflection".into(), true.into());
+                        material.set_shader_param("arg_reflection".into(), texture.into());
+                    } else {
+                        godot_warn!("Material {} has invalid bitmap", matdata.texture_reflection);
+                    }
+                } else {
+                    godot_warn!("Material {} could not be found", matdata.texture_reflection);
+                }
             }
         })
         .unwrap();
+    material.set_shader_param(
+        "arg_color".into(),
+        Vector3::new(matdata.color[0], matdata.color[1], matdata.color[2]).to_variant(),
+    );
+    material.set_shader_param("arg_alpha".into(), matdata.color[3].to_variant());
+    let mat = &matdata.transform.mat;
+    let tx = Transform {
+        basis: Basis {
+            elements: [
+                Vector3::new(mat[0], mat[1], mat[2]),
+                Vector3::new(mat[3], mat[4], mat[5]),
+                Vector3::new(mat[6], mat[7], mat[8]),
+            ],
+        },
+        origin: Vector3::new(0.0, 0.0, 0.0),
+    };
+    material.set_shader_param("arg_texcoord_transform".into(), tx.to_variant());
     Some(material.to_reference())
 }
 

1e23c19293c9c0453861e71204f0ac1a57b864b2
Jocelyn Beedie <jellonator00@gmail.com>
2020-07-30 17:53:44 -0400

    Added icons to nodes

--- Gui/Worldview/NodeTree.gd
+++ Gui/Worldview/NodeTree.gd
@@ -1,6 +1,38 @@
 extends Tree
 
 var added_nodes = {}
+const NODE_TYPE_ICONS = {
+	"SURFACE": preload("res://Gui/Icon/surface.png"),
+	"SPLINE": preload("res://Gui/Icon/spline.png"),
+	"SKIN": preload("res://Gui/Icon/skin.png"),
+	"ROTSHAPE": preload("res://Gui/Icon/rotshape.png"),
+	"LOD": preload("res://Gui/Icon/lod.png"),
+	"MESH": preload("res://Gui/Icon/mesh.png"),
+	"CAMERA": preload("res://Gui/Icon/camera.png"),
+	"CAMERAZONE": preload("res://Gui/Icon/camerazone.png"),
+	"LIGHT": preload("res://Gui/Icon/light.png"),
+	"HFOG": preload("res://Gui/Icon/hfog.png"),
+	"COLLISIONVOL": preload("res://Gui/Icon/collisionvol.png"),
+	"OMNI": preload("res://Gui/Icon/omni.png"),
+	"PARTICLES": preload("res://Gui/Icon/particles.png"),
+}
+
+const ICON_ROOT := preload("res://Gui/Icon/root.png")
+const ICON_NODE := preload("res://Gui/Icon/node.png")
+
+func _sort_nodes(a, b):
+	if len(a["children"]) != len(b["children"]):
+		return len(a["children"]) > len(b["children"])
+	if a["type"] != b["type"]:
+		return a["type"] < b["type"]
+	return a["name"] < b["name"]
+
+func get_node_icon(node):
+	if node["parent"] == 0:
+		return ICON_ROOT
+	if node["type"] in NODE_TYPE_ICONS:
+		return NODE_TYPE_ICONS[node["type"]]
+	return ICON_NODE
 
 func add_node(node, item_parent):
 	if node in added_nodes:
@@ -12,6 +44,8 @@ func add_node(node, item_parent):
 	item.set_text(0, node["name"])
 	item.set_custom_color(0, Color.white)
 	item.set_meta("node", node)
+	item.set_icon(0, get_node_icon(node))
+	node["children"].sort_custom(self, "_sort_nodes")
 	for child in node["children"]:
 		add_node(child, item)
 
